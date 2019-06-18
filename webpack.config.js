8a872cb3a9e5b0d1c2806ace7419fb178ba02f79
phongnd <nguyenphong.hanu@gmail.com>
2019-06-18 17:19:05 +0900

    edit webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     publicPath: '/',
-    libraryTarget: 'commonjs'
+    libraryTarget: 'commonjs2'
   },
   target: 'node',
   devtool: 'cheap-eval-source-map',
